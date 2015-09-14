acb14c2f396e04d6048e62ac1f3ca4954e645a9d
jbenkual <joe@benkual.com>
2015-09-14 16:22:06 -0700

    Add TweetCtrl controller for textarea

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
-//import angular from "angular";
+import angular from "angular";
 require('../css/index.css');
+
+let app = angular.module("twitterClone", []);
+
+app.controller("TweetCtrl", function () {
+	this.tweet = '';
+
+	this.invalidTweetLength = function () { 
+		return this.tweet.length === 0 || this.tweet.length > 140;
+	}
+});
