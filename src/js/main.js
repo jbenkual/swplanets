76dbaa96b2b842af81721586b3a1865588d17af8
jbenkual <joe@benkual.com>
2015-09-15 00:55:51 -0700

    Use Star Wars API to get planet data

--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,24 @@
+/* esnext: true*/
 'use strict';
 import angular from "angular";
 require('../css/index.css');
 
-let app = angular.module("twitterClone", []);
+let app = angular.module("StarWars", []);
 
-app.controller("TweetCtrl", function ($scope, $http) {
-	$scope.tweet = '';
-
-	$scope.invalidTweetLength = () => $scope.tweet.length === 0 || $scope.remainingCharacters() < 0;
-	$scope.remainingCharacters = () => 140 - $scope.tweet.length;
-
-	$scope.getIMG = () => {
-		$http.get("https://www.reddit.com/r/perfectloops.json").success( (data) => {
-			let item =  data.data.children[Math.floor(Math.random() * data.data.children.length-1)+1].data;
-			$scope.LOOP = item.url;
+app.controller("swctrl", function ($scope, $http) {
+	$scope.planetList = [];
+	let count = 0;
+	$scope.getData = (url) => {
+		console.log("loading info");
+		$http.get(url).success( (data) => {
+			console.log(data);
+			$scope.planetList.push.apply($scope.planetList, data.results);
+			if(data.next !== "null") {
+				$scope.getData(data.next);
+			}
 		});
 	};
 
-	$scope.tweetList = [];
-	$scope.sendTweet = () => {
-		$scope.tweetList.unshift($scope.tweet);
-		if($scope.tweet.match(/(?:\b|^)\#perfectloop(?:\b|$)/)) {
-			$scope.getIMG();
-			//console.log("test!");
-		}
-	};
-	$scope.tweet = '';
+	$scope.getData("http://swapi.co/api/planets/");
 
-	$scope.LOOP = "http://i.imgur.com/6dMSUzb.gif";
-
-	$scope.tweetsEmpty = () => $scope.tweetList.length <= 0;
-
-	$scope.hide = true;
-});
-
-app.directive('myEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.myEnter);
-                });
-
-                event.preventDefault();
-            }
-        });
-    };
 });
